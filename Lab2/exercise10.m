af69c972810f5fe7ce7919df6c7036ced8e281ef
Youssef Beltagy <potoato@hotmail.com>
2021-11-07 12:35:17 -0800

    Lab2/Convolution Code + Images

---
+++ Lab2/exercise10.m
@@ -0,0 +1,40 @@
+
+% Time Signal Parameters
+del = 0.001;
+st = -5;
+ed = 10;
+
+% Generate time signal
+t = st:del:ed;
+
+% Generate two box plots
+h = boxt(t, 0, 4); % (0,4)
+x = boxt(t, -1, 1); % (-1,1)
+
+% Generate output
+y = conv(x,h,'full')*del;
+% multiply by del to compensate for the fact a continous signal
+% is represented in discrete format
+
+
+% Plotting
+layout = tiledlayout(3,1);
+title(layout, "Exercise 10");
+
+nexttile
+plot(t, h);
+title("h(t)");
+ylabel("amplitude");
+xlabel ("time");
+
+nexttile
+plot(t,x)
+title("x(t)")
+ylabel("amplitude");
+xlabel ("time");
+
+nexttile
+plot(2*st:del:2*ed,y)
+title("conv(x,h)")
+ylabel("amplitude");
+xlabel ("time");
