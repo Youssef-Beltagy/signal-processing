5420979e815638160872ddde7754e1105fb00cf5
Youssef Beltagy <potoato@hotmail.com>
2021-12-08 16:36:39 -0800

    Updated Code + Refactored Project
     - Added vocode_run for easy execution
     - Added some code to print intermediate results --> needs to clean it up.

--- vocoder/vocode.m
+++ vocoder/vocode.m
@@ -1,8 +1,15 @@
-function os = vocode(ss, Fs, num_bands)
+function os = vocode(ss, Fs, num_bands, fmin, fmax)
+   arguments
+        ss(:,:);
+        Fs(1,1);
+        num_bands(1,1) = 8;
+        fmin(1,1) = 300;
+        fmax(1,1) = 6000;
+   end
 
-N = size(ss);
+SIZE = size(ss);
 
-if N(1) > 1
+if SIZE(1) > 1
     ss = ss';
 end
 
@@ -11,7 +18,7 @@ t = ( 0:(N - 1) ) ./ Fs;
 
 [lpf_bb,lpf_aa]=butter(2,400/(Fs/2));
 
-cutoffs = bands_cutoff(300, 6000, num_bands);
+cutoffs = bands_cutoff(fmin, fmax, num_bands);
 
 os = zeros(1, N); % output signal
 
@@ -20,7 +27,10 @@ for i=1:num_bands
     [bpf_bb,bpf_aa]=butter(3,[cutoffs(i) cutoffs(i+1)] ./ (Fs/2));
     
     band = filter(bpf_bb, bpf_aa, ss);% Get Band
-        
+    
+    %TODO: Delete me later
+    frequency_band = band;
+    
     %Generate Envelope
     envelope = abs(band);
     envelope = filter(lpf_bb, lpf_aa, envelope); %TODO: should I half the frequency
@@ -30,9 +40,97 @@ for i=1:num_bands
     
     band = filter(bpf_bb, bpf_aa, band);% pass band through BPF again
     
+    %TODO: delete me later
+    %plot_fft(ss, Fs, bpf_bb, bpf_aa, frequency_band, envelope, band, cutoffs(i), cutoffs(i+1));
+        
     os = os + band;
 end
 
-os = os ./ max(os); %normalize
+os = os ./ max(abs(os)); %normalize
+
+if SIZE(1) > 1
+    os = os';
+end
+
+end
+
+% Generates Frequency Cutoffs for the bands
+function fco=bands_cutoff(fmin, fmax, N)
+
+  % TODO: This "relative value" logarithmic seems
+  % meaningless to me.
+  % Investigate and possibly eliminate later.
+  xmin = log10(fmin/165.4+1)/2.1;
+  xmax = log10(fmax/165.4+1)/2.1;    %relative value
+
+  dx = (xmax-xmin)/N;
+  
+  x = xmin:dx:xmax;
+  
+  fco=zeros(1,N+1); 
+  
+for i=1:N+1
+
+fco(i)=165.4*(10^(x(i)*2.1)-1);
+
+end
+end
+
+%Plots intermediary results
+function plot_fft(ss,Fs,bb,aa,band_ss, envelope, result, fmin, fmax)
+
+N = length(ss);
+f = ((-N/2+1):(N/2)) .* (Fs / N);
+t = (0:(N-1)) ./ Fs;
+
+% plot
+figure();
+layout = tiledlayout(5,1);
+title(layout, sprintf("BPF for %d - %d", round(fmin), round(fmax)));
+
+nexttile
+FT_ss = fftshift(fft(ss));
+plot(f,abs(FT_ss));
+title("original signal");
+ylabel("Amplitude");
+xlabel("Frequency / Hz");
+xlim([0 5000]);
+ylim([0,200]);
+
+
+nexttile
+[H,F]=freqz(bb,aa,256,Fs); 
+plot(F,abs(H));
+title("BPF Filter");
+ylabel("Amplitude");
+xlabel("Frequency / Hz");
+xlim([0 5000]);
+ylim([0,1]);
+
+nexttile
+FT_band_ss = fftshift(fft(band_ss));
+plot(f,abs(FT_band_ss));
+title("Frequency Band (BPF result)");
+ylabel("Amplitude");
+xlabel("Frequency / Hz");
+xlim([0 5000]);
+ylim([0,200]);
+
+nexttile;
+plot(t,envelope);
+title("Envelope of Band");
+ylabel("Amplitude");
+xlabel("Time / s");
+xlim([0.2 1]);
+ylim([-0.1 0.1]);
+
+nexttile;
+plot(t,result);
+title("Band");
+ylabel("Amplitude");
+xlabel("Time / s");
+xlim([0.2 1]);
+ylim([-0.1 0.1]);
+
 
 end
--- vocoder/vocode_run.m
+++ vocoder/vocode_run.m
@@ -1,11 +1,60 @@
-for i=1:15
-[ss,Fs] = audioread(sprintf('sent%03d.wav',i));
-play_sound(ss,Fs);
+function vocode_run(i, mode, num_bands)
+    arguments
+        i(1,1) int8 = 0;
+        mode(1,1) string = "both";
+        num_bands(1,1) = 8;
+    end
+
+% Choose Mode
+% defualt is to play both original and vocoded sound
+play = @play_sound; 
+
+if(strcmp(mode,'original'))
+    play = @play_original;
+elseif(strcmp(mode,'vocode'))
+    play = @play_vocode;
+elseif(strcmp(mode,'reverse'))
+    play = @play_sound_rev;
 end
 
-function play_sound(ss,Fs)
+% Play Sound    
+if i == 0
+    for n=1:25
+    [ss,Fs] = audioread_mono(n);
+    play(ss, Fs, num_bands);
+    end
+elseif i > 0 && i <= 25
+    [ss, Fs] = audioread_mono(i);
+    play(ss, Fs, num_bands);
+end
+    
+end
+
+function [ss,Fs] = audioread_mono(i)
+    [ss, Fs] = audioread(sprintf('sounds/sent%03d.wav',i));
+    
+    SIZE = size(ss);
+    if(SIZE(2) > 1)
+        ss = (ss(:,1) + ss(:,2)) ./ 2;
+    end
+end
+
+function play_original(ss, Fs, num_bands)
 sound(ss,Fs);
 pause(length(ss)/Fs + 0.5);
-sound(vocode(ss,Fs,8),Fs);
+end
+
+function play_vocode(ss,Fs, num_bands)
+sound(vocode(ss,Fs,num_bands),Fs);
 pause(length(ss)/Fs + 0.5);
+end
+
+function play_sound(ss, Fs, num_bands)
+    play_original(ss,Fs, num_bands);
+    play_vocode(ss,Fs, num_bands);
+end
+
+function play_sound_rev(ss, Fs, num_bands)
+    play_vocode(ss,Fs, num_bands);
+    play_original(ss,Fs, num_bands);
 end
